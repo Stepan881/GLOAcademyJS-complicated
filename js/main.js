e239d18ae182689b84abef0f200c19c46df22743
stepan881 <stepanstepanov180@gmail.com>
2020-04-01 12:02:16 +0300

    Урок 5 простые числа!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log(1);
+"use strict";
+
+let arr = ['123', '234', '345', '456', '567', '678', '789', '890'];
+
+arr.forEach(element => {
+  if (element[0] === '2' || element[0] === '4') {
+    console.log(element);
+  }
+});
+
+nextPrime:
+for (let i = 2; i < 100; i++) {
+  for (let k = 2; k < i; k++) {
+    if (i % k === 0) {
+      continue nextPrime;
+    }    
+  }
+  console.log(i + ': делитель этого числа 1 и ' + i);
+}
+
