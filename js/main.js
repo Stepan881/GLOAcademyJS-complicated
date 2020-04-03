c4289412cc5396614bd7191d293da69036335000
stepan881 <stepanstepanov180@gmail.com>
2020-04-03 15:55:53 +0300

    lesson7 nwe Date

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+
+let week = ['пн','вт','ср','чт','пт','сб','вс' ];
+let date = new Date().getDay();
+
+week.splice(date-1, 1 , week[date-1].bold());
+week.splice(5, 2, week[date].italics(), week[date+1].italics());
+console.log(week.join('\n'));
+
+
