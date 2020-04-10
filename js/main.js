ba798cbea9f6f268484a6cbe36d004e15f2dfd1f
stepan881 <stepanstepanov180@gmail.com>
2020-04-10 14:47:10 +0300

    lesson12 Random color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const btn = document.querySelector('.btn');
+const head = document.querySelector('.head');
+const body = document.querySelector('.body');
+
+btn.addEventListener('click', ()=> {
+  let color = '#' + (Math.random().toString(16) + '000000').substring(2,8);
+  body.style.backgroundColor = color;
+  head.textContent = color;
+});
